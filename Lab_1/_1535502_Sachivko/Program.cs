0f94dd3ecf9a9eb6ccb220b8c8c777b0dfd13a4c
ноут <@DESKTOP-GENO4QG>
2022-03-10 13:34:32 +0300

    Добавьте файлы проекта.

---
+++ Lab_1/_1535502_Sachivko/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _153502_Sachivko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            double res = (double) a / b;
+            Console.WriteLine(res);
+
+        }
+    }
+}
